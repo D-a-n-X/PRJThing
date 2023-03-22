65b3e6dcac2f515d40d24b9f6dbbb68099b2dd33
CanhDD <canhddhe170593@fpt.edu.vn>
2023-03-23 03:23:14 +0700

    adsasdasdasdasdas

--- src/java/dal/StudentDBContext.java
+++ src/java/dal/StudentDBContext.java
@@ -205,6 +205,56 @@ public class StudentDBContext extends DBContext<Student>{
         }
         return student;
     }
+    public ArrayList<Student> searchBySes(int session) {
+          ArrayList<Student> students = new ArrayList<>();
+          PreparedStatement stm = null;
+          ResultSet rs = null;
+          try {
+               String sql = "SELECT s.[sid], s.scode, s.sname, s.img, s.mail, s.contact, s.gender, s.dob, s.accid\n"
+                       + "FROM Student s INNER JOIN StudentGroup sg\n"
+                       + "ON s.[sid] = sg.[sid]\n"
+                       + "INNER JOIN [Group] g ON g.gid = sg.gid\n"
+                       + "INNER JOIN [Session] ses ON ses.gid = g.gid\n"
+                       + "WHERE ses.sessionid = ?\n";
+               stm = connection.prepareStatement(sql);
+               stm.setInt(1, session);
+               rs = stm.executeQuery();
+
+               while (rs.next()) {
+                    Student s = new Student();
+                    s.setId(rs.getInt("sid"));
+                    s.setCode(rs.getString("scode"));
+                    s.setName(rs.getString("sname"));
+                    s.setImg(rs.getString("img"));
+                    s.setMail(rs.getString("mail"));
+                    s.setContact(rs.getInt("contact"));
+                    s.setGender(rs.getBoolean("gender"));
+                    s.setDob(rs.getDate("dob"));
+
+                    students.add(s);
+               }
+          } catch (SQLException ex) {
+               Logger.getLogger(StudentDBContext.class.getName()).log(Level.SEVERE, null, ex);
+          } finally {
+               try {
+                    rs.close();
+               } catch (SQLException ex) {
+                    Logger.getLogger(StudentDBContext.class.getName()).log(Level.SEVERE, null, ex);
+               }
+
+               try {
+                    stm.close();
+               } catch (SQLException ex) {
+                    Logger.getLogger(StudentDBContext.class.getName()).log(Level.SEVERE, null, ex);
+               }
+               try {
+                    connection.close();
+               } catch (SQLException ex) {
+                    Logger.getLogger(StudentDBContext.class.getName()).log(Level.SEVERE, null, ex);
+               }
+          }
+          return students;
+     }
     @Override
     public void insert(Student model) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
